3252322649cf061b14e7d7e69edbd77800551272
nisio <nisiosh@users.noreply.github.com>
2026-06-27 20:48:45 +0100

    Added vote buttons and ballot json

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,28 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import VoteOptions from "$lib/components/VoteOptions.svelte";
+    import type { Ballot } from "$lib/types/OptionTypes.js";
+
+    let { data }: { data: { minecraftBallot: Ballot } } = $props();
+
+    const ballot = $derived(data.minecraftBallot);
+</script>
+
+
+
+<div class="vote-options">
+    {#each ballot.categories as category, i}
+        <VoteOptions 
+            options={category.options} 
+            title={category.title}
+            assetPath={ballot.assetPath}
+            marginTop={i == 0 ? "" : "5rem"}
+        />
+    {/each}
+</div>
+
+<style>
+    .vote-options {
+        display: flex;
+        flex-direction: column;
+    }
+</style>
